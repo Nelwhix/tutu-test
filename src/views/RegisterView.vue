b697dd24869f9cda23284dbc86fc0f706cfe4e58
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-09-18 08:22:07 +0100

    small ui fixes

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -57,15 +57,15 @@ const signUp = async () => {
                     <input v-model="formData.platform" type="text" id="platform" placeholder="Enter name" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100">
                     <span v-if="v$.platform.$error" class="text-red-400">{{ v$.platform.$errors[0].$message }}</span>
                 </div>
-                <div class="flex justify-between">
+                <div class="flex justify-between gap-3">
                     <div class="flex flex-col mt-4">
                         <label for="email" class="text-start">Email</label>
-                        <input v-model="formData.email" type="email" id="email" placeholder="example@domain.com" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100 w-72">
+                        <input v-model="formData.email" type="email" id="email" placeholder="example@domain.com" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100 w-full">
                         <span v-if="v$.email.$error" class="text-red-400">{{ v$.email.$errors[0].$message }}</span>
                     </div>
                     <div class="flex flex-col mt-4">
                         <label for="website" class="text-start">Domain</label>
-                        <input v-model="formData.domain" type="url" id="website" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100 w-68">
+                        <input v-model="formData.domain" type="url" id="website" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100 w-full">
                         <span v-if="v$.domain.$error" class="text-red-400">{{ v$.domain.$errors[0].$message }}</span>
                     </div>
                 </div>
@@ -78,7 +78,7 @@ const signUp = async () => {
                     <div class="flex flex-col mt-5 relative w-[50%]">
                         <label for="pwd" class="text-start">Password</label>
                         <input v-model="formData.password" v-if="showPassword" type="text" id="pwd" placeholder="Enter Password" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100"/>
-                        <input v-model="formData.password" v-else type="password" id="pwd" placeholder="Enter Password" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100"/>
+                        <input v-model="formData.password" v-else type="password" placeholder="Enter Password" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100"/>
                         <button @click.prevent="toggleShow1" class="absolute right-2 top-12">
                             <svg v-if="showPassword" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                             <path fill="currentColor" d="M325.1 351.5L225.8 273.6c8.303 44.56 47.26 78.37 94.22 78.37C321.8 352 323.4 351.6 325.1 351.5zM320 400c-79.5 0-144-64.52-144-143.1c0-6.789 1.09-13.28 1.1-19.82L81.28 160.4c-17.77 23.75-33.27 50.04-45.81 78.59C33.56 243.4 31.1 251 31.1 256c0 4.977 1.563 12.6 3.469 17.03c54.25 123.4 161.6 206.1 284.5 206.1c45.46 0 88.77-11.49 128.1-32.14l-74.5-58.4C356.1 396.1 338.1 400 320 400zM630.8 469.1l-103.5-81.11c31.37-31.96 57.77-70.75 77.21-114.1c1.906-4.43 3.469-12.07 3.469-17.03c0-4.976-1.562-12.6-3.469-17.03c-54.25-123.4-161.6-206.1-284.5-206.1c-62.69 0-121.2 21.94-170.8 59.62L38.81 5.116C34.41 1.679 29.19 0 24.03 0C16.91 0 9.839 3.158 5.121 9.189c-8.187 10.44-6.37 25.53 4.068 33.7l591.1 463.1c10.5 8.203 25.57 6.333 33.69-4.073C643.1 492.4 641.2 477.3 630.8 469.1zM463.1 256c0 24.85-6.705 47.98-17.95 68.27l-38.55-30.23c5.24-11.68 8.495-24.42 8.495-38.08c0-52.1-43-96-95.1-96c-2.301 .0293-5.575 .4436-8.461 .7658C316.8 170 319.1 180.6 319.1 192c0 10.17-2.561 19.67-6.821 28.16L223.6 149.9c25.46-23.38 59.12-37.93 96.42-37.93C399.5 112 463.1 176.6 463.1 256z"></path></svg>
@@ -88,8 +88,8 @@ const signUp = async () => {
                     </div>
                     <div class="flex flex-col mt-5 relative w-1/2">
                     <label for="pwd_conf" class="text-start">Confirm Password</label>
-                    <input v-model="formData.password_confirmation" v-if="showPasswordConfirmation" type="text" id="pwd" placeholder="Confirm Password" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100"/>
-                    <input v-model="formData.password_confirmation" v-else type="password" id="pwd" placeholder="Confirm Password" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100"/>
+                    <input v-model="formData.password_confirmation" v-if="showPasswordConfirmation" type="text" id="pwd_conf" placeholder="Confirm Password" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100"/>
+                    <input v-model="formData.password_confirmation" v-else type="password" id="pwd_conf" placeholder="Confirm Password" class="focus:outline focus:outline-[3px] border rounded-md pl-3 py-3 mt-2 focus:border-blue-100 focus:outline-blue-200 focus:bg-gray-100"/>
                     <button @click.prevent="toggleShow2" class="absolute right-2 top-12">
                         <svg v-if="showPasswordConfirmation" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                         <path fill="currentColor" d="M325.1 351.5L225.8 273.6c8.303 44.56 47.26 78.37 94.22 78.37C321.8 352 323.4 351.6 325.1 351.5zM320 400c-79.5 0-144-64.52-144-143.1c0-6.789 1.09-13.28 1.1-19.82L81.28 160.4c-17.77 23.75-33.27 50.04-45.81 78.59C33.56 243.4 31.1 251 31.1 256c0 4.977 1.563 12.6 3.469 17.03c54.25 123.4 161.6 206.1 284.5 206.1c45.46 0 88.77-11.49 128.1-32.14l-74.5-58.4C356.1 396.1 338.1 400 320 400zM630.8 469.1l-103.5-81.11c31.37-31.96 57.77-70.75 77.21-114.1c1.906-4.43 3.469-12.07 3.469-17.03c0-4.976-1.562-12.6-3.469-17.03c-54.25-123.4-161.6-206.1-284.5-206.1c-62.69 0-121.2 21.94-170.8 59.62L38.81 5.116C34.41 1.679 29.19 0 24.03 0C16.91 0 9.839 3.158 5.121 9.189c-8.187 10.44-6.37 25.53 4.068 33.7l591.1 463.1c10.5 8.203 25.57 6.333 33.69-4.073C643.1 492.4 641.2 477.3 630.8 469.1zM463.1 256c0 24.85-6.705 47.98-17.95 68.27l-38.55-30.23c5.24-11.68 8.495-24.42 8.495-38.08c0-52.1-43-96-95.1-96c-2.301 .0293-5.575 .4436-8.461 .7658C316.8 170 319.1 180.6 319.1 192c0 10.17-2.561 19.67-6.821 28.16L223.6 149.9c25.46-23.38 59.12-37.93 96.42-37.93C399.5 112 463.1 176.6 463.1 256z"></path></svg>
